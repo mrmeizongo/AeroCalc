fa94ad3802bb01e2b0bc1061af52156e1be60ab6
Jamal Meizongo <mrmeizongo@outlook.com>
2024-10-13 12:36:57 -0400

    Added wingload calculation with -w option

--- cli.cpp
+++ cli.cpp
@@ -1,11 +1,13 @@
 #include "cli.h"
 #include <string>
 
-std::string helpTxt = "Usage:\tAeroCalc [option]"
+std::string helpTxt = "Usage:\tAeroCalc [option] [...param]"
 "\n\tAeroCalc [Wingspan] [FuselageLengthModifier] [WingRootChordModifier] [WingTipChordModifier] [HStabAreaModifier] [vStabAreaModifier] [Weight]"
 "\nOptions:"
 "\n-h, --help\t\t\tPrint command line options"
-"\n\nRunning program without arguments uses default values for airplane settings";
+"\n-w, --wingload\t\t\tCalculate wingload for a given weight, wingpspan, and average mean chord parameters"
+"\n\nRunning program without arguments uses default values for airplane settings"
+"\nWeight values are in grams and length values are in inches";
 
 
 bool cli(int argc, char* argv[], PlaneSettings& _planeSettings)
@@ -16,67 +18,97 @@ bool cli(int argc, char* argv[], PlaneSettings& _planeSettings)
 		std::cout << helpTxt << std::endl;
 		return false;
 	}
+	else if (arg == "--wingload" || arg == "-w")
+	{
+		if (argc != 5)
+		{
+			std::cout << helpTxt << std::endl;
+			return false;
+		}
+		float weight = (float)std::atof(argv[2]);
+		float wingspan = (float)std::atof(argv[3]);
+		float averageMeanChord = (float)std::atof(argv[4]);
+		float wingSurfArea = wingspan * averageMeanChord;
+		float wingLoad = (weight / 28.35f) / (wingSurfArea / 144);
+		std::cout << "Wing load: " << wingLoad << " oz/ft^2" << std::endl;
+		return false;
+	}
 	else if (argc > 8)
 	{
 		std::cerr << "Arguments cannot exceed 7" << std::endl;
 		std::cout << helpTxt << std::endl;
 		return false;
 	}
-	else if (argc == 2)
+	else
 	{
-		float wingspan = (float)std::atof(argv[1]);
-		_planeSettings = PlaneSettings(wingspan);
-	}
-	else if (argc == 3)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier);
-	}
-	else if (argc == 4)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		float wingRootChordModifier = (float)std::atof(argv[3]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier);
-	}
-	else if (argc == 5)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		float wingRootChordModifier = (float)std::atof(argv[3]);
-		float wingTipChordModifier = (float)std::atof(argv[4]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier);
-	}
-	else if (argc == 6)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		float wingRootChordModifier = (float)std::atof(argv[3]);
-		float wingTipChordModifier = (float)std::atof(argv[4]);
-		float hStabAreaModifier = (float)std::atof(argv[5]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier);
-	}
-	else if (argc == 7)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		float wingRootChordModifier = (float)std::atof(argv[3]);
-		float wingTipChordModifier = (float)std::atof(argv[4]);
-		float hStabAreaModifier = (float)std::atof(argv[5]);
-		float vStabAreaModifier = (float)std::atof(argv[6]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier, vStabAreaModifier);
-	}
-	else if (argc == 8)
-	{
-		float wingspan = (float)std::atof(argv[1]);
-		float fuseLenModifier = (float)std::atof(argv[2]);
-		float wingRootChordModifier = (float)std::atof(argv[3]);
-		float wingTipChordModifier = (float)std::atof(argv[4]);
-		float hStabAreaModifier = (float)std::atof(argv[5]);
-		float vStabAreaModifier = (float)std::atof(argv[6]);
-		float weight = (float)std::atof(argv[7]);
-		_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier, vStabAreaModifier, weight);
+		switch (argc)
+		{
+		case 2:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			_planeSettings = PlaneSettings(wingspan);
+			break;
+		}
+		case 3:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier);
+			break;
+		}
+		case 4:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			float wingRootChordModifier = (float)std::atof(argv[3]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier);
+			break;
+		}
+		case 5:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			float wingRootChordModifier = (float)std::atof(argv[3]);
+			float wingTipChordModifier = (float)std::atof(argv[4]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier);
+			break;
+		}
+		case 6:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			float wingRootChordModifier = (float)std::atof(argv[3]);
+			float wingTipChordModifier = (float)std::atof(argv[4]);
+			float hStabAreaModifier = (float)std::atof(argv[5]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier);
+			break;
+		}
+		case 7:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			float wingRootChordModifier = (float)std::atof(argv[3]);
+			float wingTipChordModifier = (float)std::atof(argv[4]);
+			float hStabAreaModifier = (float)std::atof(argv[5]);
+			float vStabAreaModifier = (float)std::atof(argv[6]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier, vStabAreaModifier);
+			break;
+		}
+		case 8:
+		{
+			float wingspan = (float)std::atof(argv[1]);
+			float fuseLenModifier = (float)std::atof(argv[2]);
+			float wingRootChordModifier = (float)std::atof(argv[3]);
+			float wingTipChordModifier = (float)std::atof(argv[4]);
+			float hStabAreaModifier = (float)std::atof(argv[5]);
+			float vStabAreaModifier = (float)std::atof(argv[6]);
+			float weight = (float)std::atof(argv[7]);
+			_planeSettings = PlaneSettings(wingspan, fuseLenModifier, wingRootChordModifier, wingTipChordModifier, hStabAreaModifier, vStabAreaModifier, weight);
+			break;
+		}
+		default:
+			break;
+		}
 	}
 
 	return true;
